4c6f3e149ce2d1ce3a14b68de5ef0b3b26caa2c0
uriel-200931 <uriel.maldonadocortez@utxicotepec.edu.mx>
2023-09-21 20:42:17 -0600

    Primer Cambio

---
+++ README.md
@@ -0,0 +1,41 @@
+# practica7_200931 - Widget de Botón Personalizado Y Carrusel de imagenes
+![Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png](https://i.postimg.cc/15q3LFXF/Banner-de-Twitch-Nubes-Gamer-Chica-Morado.png)
+## Información General
+
+- **Creado por:** Uriel Maldonado Cortez
+- **Asignatura:** Desarrollo Movil Integral
+- **Grado:** 10
+- **Grupo:** A
+- **Docente:** MTI. Marco Antonio Ramirez Hernandez
+# Botón Personalizado y Carrusel de Imágenes en Flutter
+
+Este proyecto de Flutter es un ejemplo simple de una aplicación que muestra un botón personalizado y un carrusel de imágenes. El botón personalizado cambia su texto cada vez que se presiona, y el carrusel de imágenes permite al usuario desplazarse a través de una serie de imágenes. La aplicación utiliza un StatefulWidget para mantener el estado interno y demostrar cómo actualizar la interfaz de usuario en respuesta a las interacciones del usuario.
+
+## Capturas de Pantalla
+
+<p align="center">
+  <img src="./assets/a.jpeg" width="200" alt="Captura de Pantalla 1">
+  <img src="./assets/b.jpeg" width="200" alt="Captura de Pantalla 2">
+</p>
+
+## Funcionalidades Principales
+
+- **Botón Personalizado**: La aplicación muestra un botón personalizado con un texto inicial de "Ara Ara". Cada vez que se presiona el botón, el texto cambia a partir de una lista predefinida de textos, incluyendo "Ara Ara", "YameteKudasai" y "Arigato".
+
+- **Carrusel de Imágenes**: La aplicación presenta un carrusel de imágenes que permite al usuario desplazarse a través de una serie de imágenes. Las imágenes se cargan desde una lista de rutas de archivo predefinidas.
+
+## Requisitos de Instalación
+
+Antes de ejecutar esta aplicación, asegúrate de tener Flutter y Dart instalados en tu sistema. Luego, sigue estos pasos:
+
+1. Clona este repositorio o descarga el código fuente.
+
+2. Abre una terminal en la carpeta del proyecto.
+
+3. Ejecuta el comando `flutter pub get` para obtener las dependencias necesarias.
+
+4. Ejecuta `flutter run` para iniciar la aplicación en un emulador o dispositivo físico.
+
+
+
+¡Gracias por explorar esta aplicación de ejemplo de Flutter! Si tienes alguna pregunta o necesitas más información, no dudes en ponerte en contacto con el autor del proyecto. ¡Diviértete desarrollando aplicaciones móviles con Flutter!
---
+++ lib/src/button.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+// Definición de la clase MyButton como StatefulWidget
+class MyButton extends StatefulWidget {
+  @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+// Declaración de variables globales
+String textButton = "Ara Ara"; // Texto inicial del botón
+int index = 0; // Índice inicial de la colección de textos
+List<String> collections = [
+  "Ara Ara",
+  "YameteKudasai",
+  "Arigato"
+]; // Lista de textos
+
+// Definición de la clase _MyButtonState que extiende State<MyButton>
+class _MyButtonState extends State<MyButton> {
+  int currentIndex = 0; // Índice inicial para el carrusel de imágenes
+  List<String> imagePaths = [
+    "assets/2.png",
+    "assets/3.png",
+    "assets/6.png",
+  ]; // Lista de rutas de imágenes
+
+  // Método que se ejecuta al presionar el botón
+  void onPressedButton() {
+    setState(() {
+      currentIndex = (currentIndex + 1) %
+          imagePaths.length; // Cambiar imagen en el carrusel
+    });
+    textButton = collections[index]; // Actualizar el texto del botón
+    index = index < collections.length - 1
+        ? index + 1
+        : 0; // Cambiar índice en la colección de textos
+  }
+
+  // Método que construye la interfaz de usuario de la aplicación
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tarea5_200931"), // Título de la barra de navegación
+      ),
+      body: Container(
+        color: Colors.black, // Fondo negro para el cuerpo de la aplicación
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 255, 255, 255),
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 255, 255, 255).withOpacity(0.5),
+                    spreadRadius: 10,
+                    blurRadius: 7,
+                    offset: Offset(0, 5),
+                  ),
+                ],
+              ),
+              margin: EdgeInsets.all(50.0),
+              padding: EdgeInsets.all(20.0),
+              child: CarouselSlider.builder(
+                itemCount:
+                    imagePaths.length, // Número de elementos en el carrusel
+                options: CarouselOptions(
+                  viewportFraction: 1.0, // Muestra una sola imagen a la vez
+                  enableInfiniteScroll:
+                      false, // Deshabilita el desplazamiento infinito
+                  onPageChanged: (index, reason) {
+                    onPressedButton(); // Llama al método al cambiar de página en el carrusel
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
+                ),
+                itemBuilder: (context, index, realIndex) {
+                  return Image.asset(
+                    imagePaths[
+                        index], // Carga la imagen desde la lista de rutas
+                    fit: BoxFit.cover,
+                  );
+                },
+              ),
+            ),
+            SizedBox(height: 30.0), // Espacio en blanco
+            Image.asset(
+              imagePaths[currentIndex], // Muestra la imagen actual del carrusel
+              fit: BoxFit.cover,
+              width: 200.0, // Ancho deseado para la imagen actual
+              height: 200.0, // Altura deseada para la imagen actual
+            ),
+            Text(
+              textButton, // Muestra el texto actual del botón
+              style: TextStyle(fontSize: 35.0, color: Colors.white),
+            ),
+            ElevatedButton(
+              onPressed:
+                  onPressedButton, // Llama al método al presionar el botón
+              child: Text("SIGUIENTE IMAGEN"), // Texto del botón
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
