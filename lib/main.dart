c6abeb648b5e9647fe84e15572191abd2993da4a
UTXJ Github <84731090+Uriel-200931@users.noreply.github.com>
2023-09-21 20:44:49 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// Creado por: Uriel Maldonado Cortez
+// Asignatura: Desarrollo Movil Integral
+//Grado: 10   Grupo: "A"
+// Docente: MTI. Marco Antonio Ramirez Hernandez
 import 'package:flutter/material.dart';
 import 'package:tarea5_200931/src/button.dart';
 
